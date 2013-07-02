3696b8bdb1f99ca0749add5cc2923ca0e42698cd
Alex Bykhun <theisease@gmail.com>
2013-07-02 23:42:52 +0400

    Changed the botKernel functionality
    Don't touch this no more >:(

--- SuperMegaBitCoinBot/botcore.cpp
+++ SuperMegaBitCoinBot/botcore.cpp
@@ -1,6 +1,21 @@
 #include <QApplication>
+#define BUY +1
+#define SELL -1
+
 
 int botRun ()
 {
     return 0;
+}
+
+int botKernel (int * lowPrices) {
+    int decision = 0;
+
+    if (lowPrices[0] < lowPrices[1])
+        decision = BUY;
+    else if (lowPrices[0] > lowPrices[1])
+        decision = SELL;
+
+
+    return decision;
 }
