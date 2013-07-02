2f2b7b8dd7a0df0948c301de34a1a628d830dde6
haumave <maxava@ukr.net>
2013-07-02 22:04:20 +0300

    Added methods

--- SuperMegaBitCoinBot/webinterface.h
+++ SuperMegaBitCoinBot/webinterface.h
@@ -5,6 +5,12 @@ class WebInterface
 {
 public:
     WebInterface();
+
+    void getRate ();
+    void buyBitCoins ( int amount );
+    void buyDollars ( int amount );
+
 };
+
 
 #endif // WEBINTERFACE_H
